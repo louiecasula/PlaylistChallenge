96ccc45942a7721d974665077a9c9c2891c2db19
louie <louiecasula@gmail.com>
2023-11-20 20:17:13 -0500

    All done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,21 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int songIndex = 0;
+        for (int i = 0; i < playList.length; i++){
+            if (playList[i].equals(selection)){
+                songIndex = i;
+            }
+        }
+        int presses = songIndex - startIndex;
+        if (presses < 0){
+            presses *= -1;
+        }
+        if (presses > playList.length / 2){
+            return playList.length - presses;
+        }
+        else{
+            return presses;
+        }
     }
 }
